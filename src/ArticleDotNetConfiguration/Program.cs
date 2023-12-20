066fe0277299690cf1f54eab635e06791bf92939
João Simões <gravity00@users.noreply.github.com>
2023-12-20 16:55:02 +0000

    project setup

---
+++ src/ArticleDotNetConfiguration/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+using var host = builder.Build();
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("Application started...");
+
+logger.LogInformation("Application terminated. Press <enter> to exit...");
+Console.ReadLine();
