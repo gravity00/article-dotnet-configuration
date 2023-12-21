186fb46bfde386acac9e0bd4876afe498fcad52f
João Simões <gravity00@users.noreply.github.com>
2023-12-21 00:04:05 +0000

    removed logging usage

--- src/ArticleDotNetConfiguration/Program.cs
+++ src/ArticleDotNetConfiguration/Program.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -16,19 +15,15 @@ builder.Services.Configure<AppOptions>(
 
 using var host = builder.Build();
 
-var logger = host.Services.GetRequiredService<ILogger<Program>>();
-
-logger.LogInformation("Application started...");
-
 var appOptions = host.Services.GetRequiredService<IOptionsMonitor<AppOptions>>();
 
 await Task.Run(async () =>
 {
-    while (true)
+    do
     {
         var options = appOptions.CurrentValue;
 
-        logger.LogInformation("Application says: '{Message}'", options.Message);
+        Console.WriteLine("Application says: '{0}'", options.Message);
         await Task.Delay(5_000);
-    }
+    } while (true);
 });
